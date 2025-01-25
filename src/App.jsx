a36ae35560675abf152c0483bd2dcae09ba7d575
Gobi Mahato <gobimahato@gmail.com>
2025-01-25 13:51:16 +0530

    integrate Tailwind CSS v4 and add assets to public folder

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello World! from React.js
+      </h1>
+    </div>
+  );
+};
+
+export default App;
